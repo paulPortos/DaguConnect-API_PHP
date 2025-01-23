d32a99290ab7fd67162be41f13a84b44c03c9e20
Auxilium <PaulVillacote.PGMR@gmail.com>
2025-01-23 22:50:25 +0800

    as

--- readme.php
+++ readme.php
@@ -32,6 +32,7 @@
     id INT(11) AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
     age INT(11) NOT NULL,
     suspend TINYINT(1) NOT NULL,
     email_verified_at DATE NULL,
