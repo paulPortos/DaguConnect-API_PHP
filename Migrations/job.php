6c802d05f425736223574b46d7804bae5ed1339e
Auxilium <PaulVillacote.PGMR@gmail.com>
2025-01-26 12:31:17 +0800

    Create job.php

---
+++ Migrations/job.php
@@ -0,0 +1,25 @@
+CREATE TABLE jobs (
+id INT UNSIGNED AUTO_INCREMENT,
+user_id INT UNSIGNED NOT NULL,
+client_fullname VARCHAR(255) NOT NULL,
+salary DECIMAL(10,2) NOT NULL,
+job_type ENUM(
+'carpentry',
+'painting',
+'welding',
+'electrical_work',
+'plumbing',
+'masonry',
+'roofing',
+'ac_repair',
+'mechanics',
+'drywalling',
+'glazing'
+) NOT NULL,
+job_description TEXT NOT NULL,
+status ENUM('available','ongoing','done') NOT NULL,
+deadline DATE NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (id),
+FOREIGN KEY (user_id) REFERENCES users(id)
+) ENGINE = InnoDB;
