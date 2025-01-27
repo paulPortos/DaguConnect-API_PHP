a11d82341add05ea5ca8f0b27c2db66262423fed
Auxilium <PaulVillacote.PGMR@gmail.com>
2025-01-27 23:14:35 +0800

    Create 08_job_application.php

---
+++ Migrations/08_job_application.php
@@ -0,0 +1,14 @@
+CREATE TABLE job_applications (
+id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+user_id INT UNSIGNED NOT NULL,
+resume_id INT UNSIGNED NOT NULL,
+job_id INT UNSIGNED NOT NULL,
+job_name VARCHAR(255) NOT NULL,
+job_type ENUM('carpentry','painting','welding','electrical_work','plumbing','masonry','roofing','ac_repair','mechanics','drywalling','glazing')
+qualification_summary TEXT NOT NULL,
+status ENUM('pending','accepted','declined'),
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (user_id) REFERENCES users(id),
+FOREIGN KEY (resume_id) REFERENCES user_resume(id)
+FOREIGN KEY (job_id) REFERENCES jobs(id)
+)
