7dbd49c361391e0d561ac118d64d71c7679730e4
NooisY <kape.rivo.up@phinmaed.com>
2025-02-18 14:38:38 +0800

    change

--- Seeders/User_Seed.php
+++ Seeders/User_Seed.php
@@ -15,102 +15,104 @@ trait User_Seed
     }
 
     public function seed_user(): void {
-        $this->seedUser1();
-        $this->seedUser2();
-        $this->seedUser3();
-        $this->seedUser4();
-        $this->seedUser5();
-        $this->seedUser6();
-        $this->seedUser7();
-        $this->seedUser8();
-        $this->seedUser9();
-        $this->seedUser10();
-        $this->seedUser11();
-        $this->seedUser12();
-        $this->seedUser13();
-        $this->seedUser14();
-        $this->seedUser15();
+        $host = $_SERVER['HTTP_HOST'] ?? 'localhost';
+        $url = "http://{$host}/uploads/profile_pictures/Default.png";
+        $this->seedUser1($url);
+        $this->seedUser2($url);
+        $this->seedUser3($url);
+        $this->seedUser4($url);
+        $this->seedUser5($url);
+        $this->seedUser6($url);
+        $this->seedUser7($url);
+        $this->seedUser8($url);
+        $this->seedUser9($url);
+        $this->seedUser10($url);
+        $this->seedUser11($url);
+        $this->seedUser12($url);
+        $this->seedUser13($url);
+        $this->seedUser14($url);
+        $this->seedUser15($url);
     }
 
-    private function seedUser1 (): void {
+    private function seedUser1 ($url): void {
         $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
         $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?,?)");
-        $stmt->execute(['Xmen', 'Wolverine', 'XBoy', '1999-03-21', 0, 'testing@gmail.com', 'http://192.168.1.157:8000/uploads/profile_pictures/Default.png' ,1, $hashed_password]);
+        $stmt->execute(['Xmen', 'Wolverine', 'XBoy', '1999-03-21', 0, 'testing@gmail.com', $url ,1, $hashed_password]);
     }
 
-    private function seedUser2 (): void {
+    private function seedUser2 ($url): void {
         $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
         $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?, ?)");
-        $stmt->execute(['Ahron Paul', 'Villacote', 'vahron24', '1999-03-21' , 0, 'vahron24@gmail.com', 'http://192.168.1.157:8000/uploads/profile_pictures/Default.png', 0, $hashed_password]);
+        $stmt->execute(['Ahron Paul', 'Villacote', 'vahron24', '1999-03-21' , 0, 'vahron24@gmail.com', $url, 0, $hashed_password]);
     }
 
-    private function seedUser3 (): void {
+    private function seedUser3 ($url): void {
         $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
         $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?,?)");
-        $stmt->execute(['Alice', 'Johnson', 'alicejohnson', '1999-03-21' , 0, 'test@gmail.com', 'http://192.168.1.157:8000/uploads/profile_pictures/Default.png', 1, $hashed_password]);
+        $stmt->execute(['Alice', 'Johnson', 'alicejohnson', '1999-03-21' , 0, 'test@gmail.com', $url, 1, $hashed_password]);
     }
-    private function seedUser4 (): void {
+    private function seedUser4 ($url): void {
         $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
         $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?,?)");
-        $stmt->execute(['test4', 'test4', 'test4', '1999-03-21' , 0, 'test4@gmail.com', 'http://192.168.1.157:8000/uploads/profile_pictures/Default.png', 0, $hashed_password]);
-    }
-
-    private function seedUser5 (): void {
-        $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
-        $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?,?)");
-        $stmt->execute(['test5', 'test5', 'test5', '1999-03-21' , 0, 'test5@gmail.com', 'http://192.168.1.157:8000/uploads/profile_pictures/Default.png', 0, $hashed_password]);
+        $stmt->execute(['test4', 'test4', 'test4', '1999-03-21' , 0, 'test4@gmail.com', $url, 0, $hashed_password]);
     }
 
-    private function seedUser6 (): void {
+    private function seedUser5 ($url): void {
         $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
         $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?,?)");
-        $stmt->execute(['test6', 'test6', 'test6', '1999-03-21' , 0, 'test6@gmail.com', 'http://192.168.1.157:8000/uploads/profile_pictures/Default.png', 0, $hashed_password]);
+        $stmt->execute(['test5', 'test5', 'test5', '1999-03-21' , 0, 'test5@gmail.com', $url, 0, $hashed_password]);
     }
 
-    private function seedUser7 (): void {
+    private function seedUser6 ($url): void {
         $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
         $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?,?)");
-        $stmt->execute(['test7', 'test7', 'test7', '1999-03-21' , 0, 'test7@gmail.com', 'http://192.168.1.157:8000/uploads/profile_pictures/Default.png', 0, $hashed_password]);
+        $stmt->execute(['test6', 'test6', 'test6', '1999-03-21' , 0, 'test6@gmail.com', $url, 0, $hashed_password]);
     }
-    private function seedUser8(): void {
+
+    private function seedUser7 ($url): void {
         $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
         $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?,?)");
-        $stmt->execute(['test8', 'test8', 'test8', '1999-03-21' , 0, 'test8@gmail.com', 'http://192.168.1.157:8000/uploads/profile_pictures/Default.png', 1, $hashed_password]);
+        $stmt->execute(['test7', 'test7', 'test7', '1999-03-21' , 0, 'test7@gmail.com', $url, 0, $hashed_password]);
     }
-    private function seedUser9(): void {
+    private function seedUser8($url): void {
         $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
         $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?,?)");
-        $stmt->execute(['test9', 'test9', 'test9', '1999-03-21' , 0, 'test9@gmail.com', 'http://192.168.1.157:8000/uploads/profile_pictures/Default.png', 0, $hashed_password]);
+        $stmt->execute(['test8', 'test8', 'test8', '1999-03-21' , 0, 'test8@gmail.com', $url, 1, $hashed_password]);
     }
-    private function seedUser10(): void {
+    private function seedUser9($url): void {
         $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
         $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?,?)");
-        $stmt->execute(['test10', 'test10', 'test10', '1999-03-21' , 0, 'test10@gmail.com', 'http://192.168.1.157:8000/uploads/profile_pictures/Default.png', 0, $hashed_password]);
+        $stmt->execute(['test9', 'test9', 'test9', '1999-03-21' , 0, 'test9@gmail.com',$url, 0, $hashed_password]);
     }
-    private function seedUser11(): void {
+    private function seedUser10($url): void {
+        $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
+        $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?,?)");
+        $stmt->execute(['test10', 'test10', 'test10', '1999-03-21' , 0, 'test10@gmail.com',$url, 0, $hashed_password]);
+    }
+    private function seedUser11($url): void {
     $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
     $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?,?)");
-    $stmt->execute(['test11', 'test11', 'test11', '1999-03-21' , 0, 'test11@gmail.com', 'http://192.168.1.157:8000/uploads/profile_pictures/Default.png', 0, $hashed_password]);
+    $stmt->execute(['test11', 'test11', 'test11', '1999-03-21' , 0, 'test11@gmail.com', $url, 0, $hashed_password]);
     }
-    private function seedUser12(): void {
+    private function seedUser12($url): void {
         $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
         $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?,?)");
-        $stmt->execute(['test12', 'test12', 'test12', '1999-03-21' , 0, 'test12@gmail.com', 'http://192.168.1.157:8000/uploads/profile_pictures/Default.png', 0, $hashed_password]);
+        $stmt->execute(['test12', 'test12', 'test12', '1999-03-21' , 0, 'test12@gmail.com', $url, 0, $hashed_password]);
     }
-    private function seedUser13(): void {
+    private function seedUser13($url): void {
         $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
         $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?,?)");
-        $stmt->execute(['test13', 'test13', 'test13', '1999-03-21' , 0, 'test13@gmail.com', 'http://192.168.1.157:8000/uploads/profile_pictures/Default.png', 0, $hashed_password]);
+        $stmt->execute(['test13', 'test13', 'test13', '1999-03-21' , 0, 'test13@gmail.com', $url, 0, $hashed_password]);
     }
-    private function seedUser14(): void {
+    private function seedUser14($url): void {
         $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
         $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?,?)");
-        $stmt->execute(['test14', 'test14', 'test14', '1999-03-21' , 0, 'test14@gmail.com', 'http://192.168.1.157:8000/uploads/profile_pictures/Default.png', 0, $hashed_password]);
+        $stmt->execute(['test14', 'test14', 'test14', '1999-03-21' , 0, 'test14@gmail.com', $url, 0, $hashed_password]);
     }
-    private function seedUser15(): void {
+    private function seedUser15($url): void {
         $hashed_password = password_hash('password123', PASSWORD_ARGON2ID);
         $stmt = $this->db->prepare("INSERT INTO users (first_name, last_name, username, birthdate, suspend, email_verified_at, email,profile, is_client, password) VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?,?)");
-        $stmt->execute(['test15', 'test15', 'test15', '1999-03-21' , 0, 'test15@gmail.com', 'http://192.168.1.157:8000/uploads/profile_pictures/Default.png', 0, $hashed_password]);
+        $stmt->execute(['test15', 'test15', 'test15', '1999-03-21' , 0, 'test15@gmail.com',$url, 0, $hashed_password]);
     }
 
 
